9a56fd7534c7f8d1df166bcb25e03610ea458f78
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2016-03-28 10:34:07 +0200

    Inline exports

--- src/index.js
+++ src/index.js
@@ -3,82 +3,82 @@ import invariant from 'invariant'
 import isDeepEqual from 'deep-eql'
 import compareErrors from 'compare-errors'
 
-const not = (test) => (obj) =>
+export const not = (test) => (obj) =>
   test({ ...obj, assert: obj.assert.not, not: true })
 
-const equal = (expected) => ({ actual, assert, stringify }) =>
+export const equal = (expected) => ({ actual, assert, stringify }) =>
   assert(actual === expected,
     `Expected ${stringify(actual)} to equal ${stringify(expected)}`,
     `Expected ${stringify(actual)} not to equal ${stringify(expected)}`,
     expected)
 
-const deepEqual = (expected) => ({ actual, assert, stringify }) =>
+export const deepEqual = (expected) => ({ actual, assert, stringify }) =>
   assert(isDeepEqual(actual, expected),
     `Expected ${stringify(actual)} to deep equal ${stringify(expected)}`,
     `Expected ${stringify(actual)} not to deep equal ${stringify(expected)}`,
     expected)
 
-const beTrue = ({ actual, assert, stringify }) =>
+export const beTrue = ({ actual, assert, stringify }) =>
   assert(actual === true,
     `Expected ${stringify(actual)} to be true`,
     `Expected ${stringify(actual)} not to be true`)
 
-const beFalse = ({ actual, assert, stringify }) =>
+export const beFalse = ({ actual, assert, stringify }) =>
   assert(actual === false,
     `Expected ${stringify(actual)} to be false`,
     `Expected ${stringify(actual)} not to be false`)
 
-const beTruthy = ({ actual, assert, stringify }) =>
+export const beTruthy = ({ actual, assert, stringify }) =>
   assert(!!actual,
     `Expected ${stringify(actual)} to be truthy`,
     `Expected ${stringify(actual)} not to be truthy`)
 
-const beFalsy = ({ actual, assert, stringify }) =>
+export const beFalsy = ({ actual, assert, stringify }) =>
   assert(!actual,
     `Expected ${stringify(actual)} to be falsy`,
     `Expected ${stringify(actual)} not to be falsy`)
 
-const beUndefined = ({ actual, assert, stringify }) =>
+export const beUndefined = ({ actual, assert, stringify }) =>
   assert(actual === undefined,
     `Expected ${stringify(actual)} to be undefined`,
     `Expected ${stringify(actual)} not to be undefined`)
 
-const beNull = ({ actual, assert, stringify }) =>
+export const beNull = ({ actual, assert, stringify }) =>
   assert(actual === null,
     `Expected ${stringify(actual)} to be null`,
     `Expected ${stringify(actual)} not to be null`)
 
-const exist = ({ actual, assert, stringify }) =>
+export const exist = ({ actual, assert, stringify }) =>
   assert(actual != null,
     `Expected ${stringify(actual)} to exist`,
     `Expected ${stringify(actual)} not to exist`)
 
-const beEmpty = ({ actual, assert, stringify }) =>
+export const beEmpty = ({ actual, assert, stringify }) =>
   assert(actual.length === 0,
     `Expected ${stringify(actual)} to be empty`,
     `Expected ${stringify(actual)} not to be empty`)
 
-const contain = (item) => ({ actual: arr, assert, stringify }) =>
+export const contain = (item) => ({ actual: arr, assert, stringify }) =>
   assert(arr.indexOf(item) > -1,
     `Expected ${stringify(arr)} to contain ${stringify(item)}`,
     `Expected ${stringify(arr)} not to contain ${stringify(item)}`)
 
-const beInstanceOf = (expected) => ({ actual, assert, stringify }) =>
+export const beInstanceOf = (expected) => ({ actual, assert, stringify }) =>
   assert(actual instanceof expected,
     `Expected ${stringify(actual)} to be instance of ${fnName(expected)}`,
     `Expected ${stringify(actual)} not to be instance of ${fnName(expected)}`)
 
-const beType = (type) => ({ actual, assert, stringify }) =>
+export const beType = (type) => ({ actual, assert, stringify }) =>
   assert(typeof actual === type,
     `Expected ${stringify(actual)} to be of type ${stringify(type)}, but was "${typeof actual}"`,
     `Expected ${stringify(actual)} not to be of type ${stringify(type)}, but was "${typeof actual}"`)
 
-const match = (regex) => ({ actual, assert, stringify }) =>
+export const match = (regex) => ({ actual, assert, stringify }) =>
   assert(regex.test(actual),
     `Expected ${stringify(actual)} to match ${stringify(regex)}`,
     `Expected ${stringify(actual)} not to match ${stringify(regex)}`)
 
-const throwError = (expected, message) => ({ actual: fn, assert, stringify }) => {
+export const throwError = (expected, message) => ({ actual: fn, assert, stringify }) => {
   invariant(typeof fn === 'function',
     `expected function as input to assertion`)
 
@@ -107,5 +107,3 @@ const throwError = (expected, message) => ({ actual: fn, assert, stringify }) =>
     `Expected function to throw`,
     `Expected function not to throw`)
 }
-
-export { not, equal, deepEqual, beTrue, beFalse, beTruthy, beFalsy, beNull, beUndefined, exist, beEmpty, contain, beInstanceOf, beType, match, throwError }
